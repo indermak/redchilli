1d3ce291359b4e565caa25a31674d9740c8ed541
megha.r <megha.rawat@successive.tech>
2019-10-09 00:30:38 +0530

    changes in footer and in contact page

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@ $(document).ready(function() {
         loop:true,
         autoplay:true,
         autoplayHoverPause: true,        
-        smartSpeed:500,          
+        smartSpeed:300,          
         margin:30,
         dots: true
     });
@@ -238,7 +238,7 @@ $(document).ready(function() {
         function init() {
             var mapOptions = {
                 zoom: 11,
-                center: new google.maps.LatLng(40.6700, -73.9400), // New York
+                center: new google.maps.LatLng(10.5663153, -61.3985823),  //Trinidad & Tobago
                 styles: [{
                     "featureType": "water",
                     "elementType": "geometry",
@@ -359,7 +359,7 @@ $(document).ready(function() {
             var mapElement = document.getElementById('map');
             var map = new google.maps.Map(mapElement, mapOptions);
             var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(40.6700, -73.9400),
+                position: new google.maps.LatLng(10.5663153, -61.3985823),
                 map: map,
                 title: 'Snazzy!'
             });
